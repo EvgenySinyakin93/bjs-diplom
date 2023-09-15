8425a88fa365b6093f2a1f10a17d54cd879d680e
Zhenya Sinyakin <evgeny.siniakin@yandex.ru>
2023-09-15 20:04:34 +0300

    Дипломное задание не готово

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,25 +1,30 @@
 "use strict"
-const { response } = require("express");
+
 
  //Подключите строгий режим выполнения кода.
 
 const userForm = new UserForm();//Создайте объект класса UserForm
-class UserForm{ 
+
     
-    loginFormCallback = (data) => { //Присвойте свойству loginFormCallback созданного...
+    userForm.loginFormCallback = (data) => { //Присвойте свойству loginFormCallback созданного...
         ApiConnector.login(data, response => { //Функция должна выполнять запрос на сервер для попытки авторизации пользователя
             if(response.success) { //Передайте в запрос авторизации функцию, которая будет выполняться при попытке авторизации.
                 location.reload(); //В случае успеха запроса обновите страницу (с помощью location.reload();).
-                return;
             } else{
-                console.log(response.error);//В случае провала запроса выведите ошибку в окно для ошибок.
-                setLoginErrorMessage(response.error);
+                userForm.setLoginErrorMessage(response.error);
             }
         })
     }
 
-    registerFormCallback(){}
+     userForm.registerFormCallback = (data) => {
+        ApiConnector.login(data, response => { //Функция должна выполнять запрос на сервер для попытки авторизации пользователя
+            if(response.success) { //Передайте в запрос авторизации функцию, которая будет выполняться при попытке авторизации.
+                location.reload(); //В случае успеха запроса обновите страницу (с помощью location.reload();).
+            } else{
+                userForm.setLoginErrorMessage(response.error);
+            }
+        })
+     }
 
-}
 
-class ApiConnector{}
+
